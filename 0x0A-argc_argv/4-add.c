8fdd0912c667ff182e52102c135634f586fffeff
Leonard Mbasela <mbaselaleon@gmail.com>
2023-07-07 02:56:37 -0700

    Task 4-add.c updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
- * Task 4 in 0x0A. C - argc, argv
- * Purpose : adds positive numbers.
- */
 
 /**
- * main - adds positive numbers.
- * @argc: argument count
- * @argv: argument vector
- * Return: (0) success or (1) faild
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int a, temp;
-	char *c;
+	int num, digit, sum = 0;
 
-	a = 0;
-
-	if (argc == 1)
+	for (num = 1; num < argc; num++)
 	{
-	printf("0\n");
-	return (0);
-	}
-	while (--argc)
+	for (digit = 0; argv[num][digit]; digit++)
 	{
-	temp = -1;
-	argv++;
-	c = *argv;
-	while ((*c) != '\0')
+	if (argv[num][digit] < '0' || argv[num][digit] > '9')
 	{
-	if (temp == -1)
-	{
-	temp = 0;
-	}
-	if ((*c >= '0') && (*c <= '9'))
-	{
-	temp = temp + (*c - '0');
-	if ((*(c + 1) != '\0'))
-	{
-	temp = temp * 10;
-	}
-	}
-	else
-	{
-        printf("Error\n");
+	printf("Error\n");
 	return (1);
 	}
-	c++;
 	}
-	a = a + temp;
+
+	sum += atoi(argv[num]);
 	}
-	printf("%d\n", a);
+
+	printf("%d\n", sum);
+
 	return (0);
 }
