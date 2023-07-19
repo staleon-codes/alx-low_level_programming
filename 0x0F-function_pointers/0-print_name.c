fe0b39ec5b1a4d60ce691726acd63309a81cc568
Leonard Mbasela <mbaselaleon@gmail.com>
2023-07-19 00:37:34 -0700

    All Tasks Completed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name.
+ * @name: char pointer to name
+ * @f: funtcion pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	f(name);
+}
