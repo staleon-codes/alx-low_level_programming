cb5ee1f87e6f08031422f7d0df8f1fda23e97090
staleon-codes <mbaselaleon@gmail.com>
2023-08-01 03:01:12 -0700

    fixed task errors

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,35 +1,28 @@
 #include "lists.h"
-
 /**
- * find_listint_loop - Finds the loop contained in
- *                     a listint_t linked list.
- * @head: A pointer to the head of the listint_t list.
+ * find_listint_loop - function that finds the loop in a linked list.
  *
- * Return: If there is no loop - NULL.
- *         Otherwise - the address of the node where the loop starts.
+ * @head: printer
+ *
+ * Return: address
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tortoise, *hare;
-	if (head == NULL || head->next == NULL)
-	return (NULL);
-	tortoise = head->next;
-	hare = (head->next)->next;
-	while (hare)
-	{
-	if (tortoise == hare)
-	{
-	tortoise = head;
-	while (tortoise != hare)
-	{
-	tortoise = tortoise->next;
-	hare = hare->next;
-	}
-	return (tortoise);
-	}
-	tortoise = tortoise->next;
-	hare = (hare->next)->next;
-	}
+	listint_t *p, *e;
 
+	if (head == NULL)
+		return (NULL);
+	for (e = head->next; e != NULL; e = e->next)
+	{
+		if (e == e->next)
+		{
+			return (e);
+		}
+		for (p = head; p != e; p = p->next)
+		{
+			if (p == e->next)
+				return (e->next);
+		}
+	}
 	return (NULL);
 }
