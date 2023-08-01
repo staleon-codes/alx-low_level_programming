db19d5fb4cdbf51450b4e31ac8893f9026404a20
staleon-codes <mbaselaleon@gmail.com>
2023-08-01 02:42:02 -0700

    fixe task 7-get_nodeint.c error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,18 +1,28 @@
 #include "lists.h"
 /**
-*get_nodeint_at_index - Locates a given node in a listint_t linked list.
-*@head: A pointer to the head of the listint_t list.
-*@index: The index of the node to locate - indices start at 0.
-*Return: If the node at the given index does not exist - NULL.
-*/
+ * get_nodeint_at_index -  returns the nth node of a listint_t
+ * @head: pointer to head in function
+ * @index: the node that will be returned
+ * Return: nth node data
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int node;
-	for (node = 0; node < index; node++)
-	{
-	if (head == NULL)
+	unsigned int counter = 0, nodes;
+	listint_t *temp = head;
+
+	if (head == NULL)/*Ask if there is not input*/
 	return (NULL);
-	head = head->next;
+	for (nodes = 0; temp != NULL; nodes++)/*Count the num of nodes*/
+	temp = temp->next;
+	if (index > nodes)
+	return (NULL);
+	temp = head;
+	while (counter <= index)
+	{
+	if (counter == index)
+	return (temp);
+	temp = temp->next;
+	counter++;
 	}
-	return (head);
+	return (NULL);
 }
