ec20de347e90e0de36ac17436578659c5657216e
staleon-codes <mbaselaleon@gmail.com>
2023-08-03 02:24:33 -0700

    Tasks complete

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,35 +1,17 @@
 #include "main.h"
 
-unsigned int _pow(int base, int pow);
 /**
- * set_bit -  change the bit value to 1 of a given index.
- * @n: array pointer to binary number.
- * @index: array pointer to binary number.
- * Return: 1 or -1 if an error occured
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the number to change
+ * @index: index of the bit to set to 1
+ *
+ * Return: 1 for success, -1 for failure
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= 64)
+	if (index > 63)
 	return (-1);
-	*n = *n + _pow(2, index);
+	*n = ((1UL << index) | *n);
 	return (1);
 }
-/**
- * _pow - calculates the power
- * @base: base
- * @pow: power
- * Return: outcome
- */
-unsigned int _pow(int base, int pow)
-{
-	unsigned int result;
-	if (pow == 0)
-	return (1);
-	if (pow == 1)
-	return (base);
-	result = base;
-	while (--pow)
-	result *= base;
-	return (result);
-}
+
