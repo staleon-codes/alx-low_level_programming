c6367c200e11c60fb459678793eebdee5b72bb29
staleon-codes <mbaselaleon@gmail.com>
2023-09-14 11:15:04 -0700

    mandatory tasks done

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - count the elements in a linked dlistint_t list.
+ * @h: pointer to dlistint_t head
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free all the elements of a dlistint_t list.
+ * @head: pointer to dlistint_t
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	current = head;
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		if (current)
+			free(current);
+	}
+}
