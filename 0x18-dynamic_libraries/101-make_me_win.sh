d72a0f86f66cc34620f3c9991bdc58b41bd12032
staleon-codes <mbaselaleon@gmail.com>
2023-09-18 03:58:44 -0700

    All taks done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -p /tmp https://github.com/staleon-codes/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/ibgiga.so
+#!/bin/bash
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/staleon-codes/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
